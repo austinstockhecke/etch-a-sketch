3e37dd5a6589e1bfe8655c3dc68f5f16dc8bed64
Austin Stockhecke <austin.stockhecke@gmail.com>
2019-08-01 21:50:20 -0500

    resize works as intended; styling / randomlize left todo

--- javascript.js
+++ javascript.js
@@ -1,32 +1,40 @@
 const contain = document.querySelector(".container");
+let numGridSquares = getComputedStyle(document.head).getPropertyValue("--gridNum");
 
 
 function createGrid(numSquares) {
-    for( let i =0; i < numSquares; i++) {
-        let col = document.createElement("div");
-        col.setAttribute("class", "col");
-        for(let j = 0; j < numSquares; j++) {
-            var row = document.createElement("div");
-            row.setAttribute("class","row");
-            
+    document.documentElement.style.setProperty("--gridNum", numSquares);
 
+    for(let i=0; i < numSquares*numSquares; i++){ 
+        let gridSquares = document.createElement("div");
+        gridSquares.setAttribute("class", "gridSquare");
 
-            col.appendChild(row);
-        }
-        contain.appendChild(col);
+        contain.appendChild(gridSquares);
 
     }
+    
+    let changeRow = document.querySelectorAll("[class=gridSquare]");
+    Object.entries(changeRow).map((object) => {
+    object[1].addEventListener("mouseenter", function(event) {
+    event.target.style.backgroundColor = "black";
+    });
+});
+}
+
+function resetGrid() {
+    document.querySelector(".container").innerHTML = "";
 }
 
 createGrid(16);
 
-let changeRow = document.querySelectorAll("[class=row]");
-console.log(changeRow);
-Object.entries(changeRow).map((object) => {
-    object[1].addEventListener("mouseenter", function(event) {
-        event.target.style.backgroundColor = "black";
 
-    });
+
+
+let resetButton = document.querySelector("[class=reset]");
+resetButton.addEventListener("click", function () {
+   let reSize = prompt("How many grid squares would you like? (5-100)");
+   resetGrid();
+   createGrid(reSize);
+    
 });
-
 
